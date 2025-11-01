52f6d36d11d20f79463264d5c1f3943810424995
efdusernet <efdusernet@gmail.com>
2025-11-01 10:53:12 -0300

    feat(db): add exam tables SQL and Sequelize models\n- SQL: exam_type, exam_attempt, exam_attempt_question, exam_attempt_answer + seed PMP\n- Models: ExamType, ExamAttempt, ExamAttemptQuestion, ExamAttemptAnswer\n- Wire associations in models/index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -13,11 +13,34 @@ const db = {};
 // Import models
 db.User = require('./User')(sequelize, DataTypes);
 db.EmailVerification = require('./EmailVerification')(sequelize, DataTypes);
+// New exam-related models
+db.ExamType = require('./ExamType')(sequelize, DataTypes);
+db.ExamAttempt = require('./ExamAttempt')(sequelize, DataTypes);
+db.ExamAttemptQuestion = require('./ExamAttemptQuestion')(sequelize, DataTypes);
+db.ExamAttemptAnswer = require('./ExamAttemptAnswer')(sequelize, DataTypes);
 
 // Associations
 if (db.User && db.EmailVerification) {
   db.EmailVerification.belongsTo(db.User, { foreignKey: 'UserId' });
   db.User.hasMany(db.EmailVerification, { foreignKey: 'UserId' });
+}
+
+// Associations for exams
+if (db.ExamType && db.ExamAttempt) {
+  db.ExamType.hasMany(db.ExamAttempt, { foreignKey: 'ExamTypeId' });
+  db.ExamAttempt.belongsTo(db.ExamType, { foreignKey: 'ExamTypeId' });
+}
+if (db.User && db.ExamAttempt) {
+  db.User.hasMany(db.ExamAttempt, { foreignKey: 'UserId' });
+  db.ExamAttempt.belongsTo(db.User, { foreignKey: 'UserId' });
+}
+if (db.ExamAttempt && db.ExamAttemptQuestion) {
+  db.ExamAttempt.hasMany(db.ExamAttemptQuestion, { foreignKey: 'AttemptId' });
+  db.ExamAttemptQuestion.belongsTo(db.ExamAttempt, { foreignKey: 'AttemptId' });
+}
+if (db.ExamAttemptQuestion && db.ExamAttemptAnswer) {
+  db.ExamAttemptQuestion.hasMany(db.ExamAttemptAnswer, { foreignKey: 'AttemptQuestionId' });
+  db.ExamAttemptAnswer.belongsTo(db.ExamAttemptQuestion, { foreignKey: 'AttemptQuestionId' });
 }
 
 db.sequelize = sequelize;
